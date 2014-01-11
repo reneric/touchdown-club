509e35067309f25d084c9edeeb05a9ab8ddb60b6
themcstaplez <matthew@gatorworks.net>
2014-01-10 22:32:14 -0600

    Add Users Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Touchdownclub::Application.routes.draw do
+  get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
-
+  match '/signup', to: 'users#new', via: 'get'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
